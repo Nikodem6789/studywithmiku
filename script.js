8b6ec6115619d469e68c40fd3ea0637fdbfb568f
Nikodem6789 <91376140+Nikodem6789@users.noreply.github.com>
2025-11-26 19:15:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-let d = document.getElementById(d)
-let ds = new Audio
-function mikudayo()
+<script>
+let sound = new Audio("mikudayoooo-burglar-mikudayo.mp3")
+
+document.getElementById("d").onclick = () => {
+    sound.currentTime = 0        // start from beginning
+    sound.play()
+    setTimeout(() => {
+        sound.pause()            // stop after 3 sec
+        sound.currentTime = 0    // rewind for next click
+    }, 3000)
+}
+</script>
