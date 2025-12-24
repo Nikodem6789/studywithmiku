bb196e7c7e622153185e1cb30802710dadaff2ae
Nikodem6789 <91376140+Nikodem6789@users.noreply.github.com>
2025-12-24 16:25:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const soundFile = "mikudayoooo-burglar-mikudayo.mp3" // just 1 media file
-
+let mikudayocounter = 0
 document.getElementById("d").onclick = () => {
     let sound = new Audio(soundFile)
     sound.currentTime = 0
@@ -7,5 +7,9 @@ document.getElementById("d").onclick = () => {
     setTimeout(() => {
         sound.pause()
         sound.currentTime = 0
-    }, 3000) // only first 3 sec
+    }, 3000)
+mikudayoocounter = mikudayoocounter + 1
+console.log("Error Detected: Miku Dayoo: Stage Cooked. You cannot disable this virus")
+// Miku Dayoo
+
 }
